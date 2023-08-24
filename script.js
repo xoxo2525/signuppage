f599fcd363cd9d87056e57b0575373f6eaca0be6
xoxo2525 <140496994+xoxo2525@users.noreply.github.com>
2023-08-25 01:06:53 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+﻿const imageContainers = document.querySelectorAll('.image-container');
+const modals = document.querySelectorAll('.modal');
+
+imageContainers.forEach((container, index) => {
+    const modal = modals[index];
+
+    container.addEventListener('mouseover', () => {
+        modal.style.display = 'block';
+    });
+
+    container.addEventListener('mouseout', () => {
+        modal.style.display = 'none';
+    });
+});
